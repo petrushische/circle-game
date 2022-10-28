bd1f48a3be334485806592ca198f20cd810ece1e
Пётр Румянцев <dogboxer64529000@gmail.com>
2022-10-28 19:56:45 +0300

    Сброс игры и возвращение на главный экран

--- index.js
+++ index.js
@@ -11,11 +11,33 @@ const board = document.querySelector('.screen__board')
 
 const buttonHome = document.querySelector('.button-home');
 
+// Возвращение домой
+function backHome() {
+ if (screens[0].classList.contains('up') && screens[1].classList.contains('up')) {
+  screens[0].classList.remove('up')
+  screens[1].classList.remove('up')
+ }
+ else if (screens[0].classList.contains('up')) {
+  screens[0].classList.remove('up')
+ }
+ else {
+  return
+ }
+}
+buttonHome.addEventListener('click', backHome)
+
 // Переход к выбору времени
 start.addEventListener('click', (evt) => {
  screens[0].classList.add('up')
 })
 
+function gameOwer() {
+ buttonHome.addEventListener('click', () => {
+  gameTime.parentNode.style.opacity = '1'
+  score = 0;
+  board.innerHTML = ``
+ })
+}
 
 // Выбор времени игры и запуск
 timeList.addEventListener('click', (evt) => {
@@ -23,6 +45,7 @@ timeList.addEventListener('click', (evt) => {
   time = Number(evt.target.id)
   screens[1].classList.add('up')
   startGame()
+  gameOwer()
  }
 })
 
@@ -35,7 +58,7 @@ function startGame() {
   } else {
    clearInterval(interval)
    finishGame()
-   gameTime.parentNode.remove()
+   gameTime.parentNode.style.opacity = '0'
   }
  }, 1000)
 }
@@ -44,23 +67,12 @@ function finishGame() {
  board.innerHTML = `<h1>Ваш счёт: ${score}</h1>`
 }
 
-
-
-board.addEventListener('click', evt => {
- if (evt.target.classList.contains('circle')) {
-  score++
-  evt.target.remove()
-  createRandomCircle()
- }
-})
-
 function createRandomCircle() {
  const circle = document.createElement('div')
  const size = getRandomNumber(15, 60)
  const { width, height } = board.getBoundingClientRect()
  const x = getRandomNumber(0, width - size)
  const y = getRandomNumber(0, height - size)
-
  circle.classList.add('circle')
  circle.style.width = `${size}px`
  circle.style.height = `${size}px`
@@ -78,3 +90,11 @@ function randomColor() {
 function getRandomNumber(min, max) {
  return Math.round(Math.random() * (max - min) + min)
 }
+
+board.addEventListener('click', evt => {
+ if (evt.target.classList.contains('circle')) {
+  score++
+  evt.target.remove()
+  createRandomCircle()
+ }
+})
